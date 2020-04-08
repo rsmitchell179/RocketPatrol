796d1bccaab3b6ca45c8f66862bdaebcda1bf235
Ryan Mitchell <rsmitche@ucsc.edu>
2020-04-08 15:47:21 -0700

    Scenes & Assets implemented

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -2,4 +2,12 @@ class Menu extends Phaser.Scene {
     constructor() {
         super("menuScene");
     }
+    
+    create() {
+        // Display Menu text
+        this.add.text(20, 20, "Rocket Patrol Menu");
+
+        // Launches the playScene
+        this.scene.start("playScene");
+    }
 }
